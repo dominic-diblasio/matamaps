f4142739474f34bd1f67024c2922f9b32346c629
vinibosque122 <157661127+vinibosque122@users.noreply.github.com>
2025-04-02 00:38:46 -0700

    Added Button to search for location onclick
    New button added to events, on click it will pass the location stored for each event to nav-map where it will be input into the search query

--- Frontend/src/screens/OtherPages/NavigationMap.js
+++ Frontend/src/screens/OtherPages/NavigationMap.js
@@ -1,17 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 
 function CSUNMapSearch() {
+  const { state: locationData } = useLocation();
   const [searchQuery, setSearchQuery] = useState("");
   const BASE_MAP_URL = "https://3dmap.csun.edu/?id=1100#!s/?sbc/";
   const SEARCH_MAP_URL = "https://3dmap.csun.edu/?id=1100#!s/";
 
-  const handleSearch = (e) => {
+
+  useEffect(() =>{
+    if(locationData){
+    setSearchQuery(locationData)
+    }
+}, [locationData])
+
+const handleSearch = (e) => {
     e.preventDefault();
     if (searchQuery.trim()) {
       document.getElementById("mm-iframe").src = `${SEARCH_MAP_URL}${encodeURIComponent(searchQuery)}`;
     }
   };
+
 
   return (
     <div className="container">
